4b460daa46d36d4e353e1fd1d423f8146910a87a
Matt Knop <mknop@redhat.com>
2023-09-26 16:09:21 -0600

    geolocation determined from user input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import axios from 'axios';
+
+export var data = {
+  city: "",
+  state: "",
+  country: "",
+  latitude: 0,
+  longitude: 0
+}
+
+export async function handleSubmit() {
+  const params = {
+    access_key: "<ENTER API KEY>",
+    query: data.city + ", " + data.state + ", " + data.country
+  }
+
+  axios.get('http://api.positionstack.com/v1/forward', {params})
+  .then(function (response) {
+      data.latitude = response.data.data[0].latitude;
+      data.longitude = response.data.data[0].longitude;
+      console.log(response)
+      console.log(data.latitude)
+      console.log(data.longitude)
+  })
+  .catch(function (error) {
+    console.log(error.toJSON());
+  })
+}
+
+</script>
+
+<div>
+  <label for="avatar">City</label>
+  <input bind:value={data.city} /><br>
+
+  <label for="avatar">State</label>  
+  <input bind:value={data.state} /><br>
+
+  <label for="avatar">Country</label>  
+  <input bind:value={data.country} /><br>
+
+  <button on:click={handleSubmit}> Find Coordinates </button>
+
+  <p>
+    Latitude: {data.longitude}<br>
+    Longitude: {data.latitude}<br>
+  </p>
+</div>
